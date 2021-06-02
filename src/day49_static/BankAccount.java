57b9c7b3ddcad75ed32f6993de09b991417ddbbb
Saba <sabageb@gmail.com>
2021-06-01 19:07:43 -0700

    Static variable and Method

---
+++ src/day49_static/BankAccount.java
@@ -0,0 +1,16 @@
+package day49_static;
+
+public class BankAccount {
+ static public double balance = 500.0;
+    String user;
+
+    public void spend(double amount){
+        System.out.println(user + " is  spending $"+ amount);
+                balance -=amount;
+
+    }
+    public void showBalance(){
+        System.out.println("Current balance ="+ balance);
+    }
+
+}
