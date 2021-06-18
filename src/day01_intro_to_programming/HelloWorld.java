0ce6c42fba98149e5987bc44936d27ab293bea91
saba <sabageb@gmail.com>
2021-06-17 19:13:16 -0700

    commit from macbook12

--- src/day01_intro_to_programming/HelloWorld.java
+++ src/day01_intro_to_programming/HelloWorld.java
@@ -1,8 +1,13 @@
 package day01_intro_to_programming;
 
-public class   HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+ public class   HelloWorld {
+public static void main(String[] args) {
+    System.out.println("Hello World!");
+}
+//    public Test() {
 
-}
-}
+    }
+
+
+//}
+
