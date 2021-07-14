0e0becba84111e983bbe6be4fb7004a7fb6151ce
Saba <sabageb@gmail.com>
2021-07-13 19:44:14 -0700

    linked classes

---
+++ src/day62_collections/VectorDemo.java
@@ -0,0 +1,26 @@
+package day62_collections;
+
+
+import java.util.*;
+
+public class VectorDemo {
+    public static void main(String[] args) {
+
+        Vector<Integer> nums = new Vector<>();
+        List <String> states = new Vector<>();
+        states.add("MA");
+        states.add("VA");
+        states.add("IL");
+        states.add("PA");
+        states.add("TX");
+        states.add("CO");
+        states.add("MO");
+        states.add("MD");
+
+        System.out.println("states");
+
+        //VECTOR IS SYNCHRONIZED
+
+
+    }
+}
